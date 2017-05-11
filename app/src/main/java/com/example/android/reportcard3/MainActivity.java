068c9ee714008e1bb153f87ba0379e6cdb6b9721
gary909 <garywhite61@hotmail.com>
2017-05-11 14:35:28 +0200

    Cleaned up code

--- app/src/main/java/com/example/android/reportcard3/MainActivity.java
+++ app/src/main/java/com/example/android/reportcard3/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.android.reportcard3;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     @Override
